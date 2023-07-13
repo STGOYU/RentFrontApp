d51064aff4b22f4a33095d7779e1df8ae4c236ed
Yunus <yunus.can42320@gmail.com>
2023-07-13 10:47:48 +0200

    13/07/2023

--- src/components/common/about/best-offers/offer-item/offer-item.jsx
+++ src/components/common/about/best-offers/offer-item/offer-item.jsx
@@ -1,5 +1,16 @@
-const OfferItem = () => {
-    return <div>OfferItem</div>;
+const OfferItem = (props) => {
+    // title, desc, direction, icon
+    return (
+        <div className={`offer-item ${props.direction}`}>
+            <div className="icon" title={props.title}>
+                {props.icon}
+            </div>
+            <div className="content">
+                <h3 className={props.direction}>{props.title}</h3>
+                <p>{props.desc}</p>
+            </div>
+        </div>
+    );
 };
 
 export default OfferItem;
--- src/components/common/spacer/spacer.jsx
+++ src/components/common/spacer/spacer.jsx
@@ -1,5 +1,4 @@
-const Spacer = () => {
-    return <div>Spacer</div>;
+const Spacer = ({ height = 100 }) => {
+    return <div style={{ height: height }}></div>;
 };
-
 export default Spacer;
