ef9b383d4c6642343aa418bcf91c58e3a6c034d7
Yunus <yunus.can42320@gmail.com>
2023-07-21 22:27:14 +0200

    21/07/2023

--- src/pages/common/login/page.jsx
+++ src/pages/common/login/page.jsx
@@ -10,85 +10,62 @@ import { CustomForm, PasswordInput } from "../../../components";
 import { constants } from "../../../constants";
 import "./style.scss";
 
-const {routes} = constants;
+const { routes } = constants;
 
 const LoginPage = () => {
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const onSubmit = async (values) => {
-    setLoading(true);
-    try {
-      // istegini endpointe gonder
-      const data = await services.user.login(values);
+    const onSubmit = async (values) => {
+        setLoading(true);
+        try {
+            // istegini endpointe gonder
+            const data = await services.user.login(values);
+            // token'i sifrelenmis localstorage'e kaydet
+            services.encryptedLocalStorage.setItem(
+                "pickanddrivetoken",
+                data.token
+            );
+            // token ile kullanici bilgilerini al
+            const responseUser = await services.user.getUser();
+            // kullanici bilgilerini merkezi state'e kaydet
+            dispatch(loginSuccess(responseUser));
+            utils.functions.swalToast(
+                "You have successfully logged in",
+                "success"
+            );
+            navigate(routes.home);
+        } catch (error) {
+            dispatch(loginFailure());
+            utils.functions.swalToast(error.response.data.message, "error");
+        } finally {
+            setLoading(false);
+        }
+    };
 
-      services.encryptedLocalStorage.setItem("pickanddrivetoken", data.token);
-      
-      // const responseUser = await services.user.getUser();
-      // token ile kullanici bilgilerini al
-      // kullanici bilgilerini merkezi state'e kaydet
-      dispatch(loginSuccess());
-      utils.functions.swalToast("You have successfully logged in", "success");
-    } catch (error) {
-      dispatch(loginFailure());
-      utils.functions.swalToast(error.response.data.message, "error");
-    } finally {
-      setLoading(false);
-    }
-  };
+    const formik = useFormik({
+        initialValues: utils.initialValues.loginFormInitialValues,
+        validationSchema: utils.validations.loginFormValidationSchema,
+        onSubmit,
+    });
 
-  const formik = useFormik({
-    initialValues: utils.initialValues.loginFormInitialValues,
-    validationSchema: utils.validations.loginFormValidationSchema,
-    onSubmit,
-  });
-
-  return (
-    <Form noValidate onSubmit={formik.handleSubmit} className="login-form">
-      {/* <Form.Group className="mb-3">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="email"
-                    placeholder="johndoe@example.com"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    isValid={
-                        formik.touched.email && !formik.errors.email
-                    }
-                    isInvalid={
-                        formik.touched.email && formik.errors.email
-                    }
-                    disabled={true}
-                />
-                <Form.Control.Feedback type="invalid">
-                    *{formik.errors.email}
-                </Form.Control.Feedback>
-
-                
-            </Form.Group> */}
-      {/* <CustomForm type="select" /> */}
-      <CustomForm
-        formik={formik}
-        name="email"
-        Label="Email Address"
-        placeholder="jhondoe@example.com"
-        type="email"
-      />
-
-      {/* PASSWORD INPUT */}
-      <PasswordInput
-        formik={formik}
-        name="password"
-        label="password"
-        placeholder="Enter your password..."
-      />
-
-      {/* CUSTOM FORM */}
-
-      <Button
+    return (
+        <Form noValidate onSubmit={formik.handleSubmit} className="login-form">
+            <CustomForm
+                formik={formik}
+                name="email"
+                label="Email Address"
+                placeholder="johndoe@example.com"
+                type="email"
+            />
+            <PasswordInput
+                formik={formik}
+                name="password"
+                label="Password"
+                placeholder="Enter your password..."
+            />
+            <Button
                 type="submit"
                 disabled={!(formik.dirty && formik.isValid) || loading}>
                 {loading && <Spinner animation="border" size="sm" />} LOGIN
--- src/services/user/user-service.js
+++ src/services/user/user-service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { encryptedLocalStorage } from "../encrypt-storage/encrypt-storage";
+import { services } from "..";
+//import { encryptedLocalStorage } from "../encrypt-storage/encrypt-storage";
 const API_URL = import.meta.env.VITE_APP_API_URL;
 
 // COMMON ENDPOINTS
@@ -14,11 +15,16 @@ export const register = async (payload) => {
 
 // USER ENDPOINTS
 export const getUser = async () => {
-    const response = await axios.get(`${API_URL}/user`, {
-        headers: {
-            Authorization: `Bearer ${encryptedLocalStorage.getItem("pickanddrivetoken")}`
-        }
-    })
+
+    const response = await axios.get(`${API_URL}/user`, services.authHeader());
+    return response.data;
+
+    // const response = await axios.get(`${API_URL}/user`, {
+    //     {
+    //       // Authorization: `Bearer ${encryptedLocalStorage.getItem("pickanddrivetoken")}`
+    //     }
+    //     return response.data;
+    // })
  };
 export const updatePassword = () => { };
 export const updateUser = () => { };
